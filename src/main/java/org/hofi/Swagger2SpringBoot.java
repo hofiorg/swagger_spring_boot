e180c7e43b165d30e0e04188b16a3583077f42f5
hofiorg <hofiorg@googlemail.com>
2018-06-21 19:25:11 +0200

    refactoring

--- src/main/java/org/hofi/Swagger2SpringBoot.java
+++ src/main/java/org/hofi/Swagger2SpringBoot.java
@@ -10,27 +10,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@ComponentScan(basePackages = { "org.hofi", "org.hofi.api", "io.swagger.configuration" })
+@ComponentScan(basePackages = {"org.hofi", "org.hofi.api", "io.swagger.configuration"})
 public class Swagger2SpringBoot implements CommandLineRunner {
 
+  @Override
+  public void run(String... arg0) {
+    if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+      throw new ExitException();
+    }
+  }
+
+  public static void main(String[] args) {
+    new SpringApplication(Swagger2SpringBoot.class).run(args);
+  }
+
+  class ExitException extends RuntimeException implements ExitCodeGenerator {
+    private static final long serialVersionUID = 1L;
+
     @Override
-    public void run(String... arg0) throws Exception {
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-            throw new ExitException();
-        }
+    public int getExitCode() {
+      return 10;
     }
 
-    public static void main(String[] args) throws Exception {
-        new SpringApplication(Swagger2SpringBoot.class).run(args);
-    }
-
-    class ExitException extends RuntimeException implements ExitCodeGenerator {
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public int getExitCode() {
-            return 10;
-        }
-
-    }
+  }
 }
